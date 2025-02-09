af211a7ae071b8c9996c71503696876d307d63a2
Dmytro Melnyk <demetrius05@proton.me>
2025-02-09 18:18:47 +0000

    Added posibility for test and better Makefile

---
+++ inc/test.h
@@ -0,0 +1,9 @@
+#ifndef TEST_H
+# define TEST_H
+
+# include <criterion/criterion.h>
+# include "minishell.h"
+# include "parser.h"
+# include "exec.h"
+
+#endif
--- shell.nix
+++ shell.nix
@@ -4,6 +4,7 @@
 pkgs.mkShell {
   packages = with pkgs; [
     readline
+    criterion
     cmake
     gcc
     bear
