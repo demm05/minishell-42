801b9cc15a7aec39df3390bd03dbe13d44ff627f
Dmytro Melnyk <dmelnyk@student.42london.com>
2025-03-04 11:55:53 +0000

    Small changes

--- src/parser/lex_generic.c
+++ src/parser/lex_generic.c
@@ -1,27 +1,16 @@
 #include "../../inc/parser.h"
-#include <stdio.h>
 
 int	is_there_exec(t_lexer *l)
 {
 	t_token				*last;
-	unsigned int		i;
-	static t_token_type	t[] = {
-		EXEC, CD, ECHO, PWD, EXPORT, UNSET,
-		ENV, EXIT};
 
 	if (!l->tokens)
 		return (0);
 	last = l->tokens->prev;
 	while (1)
 	{
-		i = 0;
-		while (i < sizeof(t) / sizeof(t[0]))
-		{
-			if (t[i++] == last->type)
-			{
-				return (1);
-			}
-		}
+		if (is_token_exec(last->type))
+			return (1);
 		if (last->type == AND || last->type == OR || last->type == PIPE)
 			break ;
 		last = last->prev;
