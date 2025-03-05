44ec0bc703cc7c96aec14aea6d4c983ccfb7329b
Dmytro Melnyk <dmelnyk@student.42london.com>
2025-03-05 19:30:58 +0000

    exit

--- src/exec/exit.c
+++ src/exec/exit.c
@@ -1,6 +1,17 @@
 #include "../../inc/exec.h"
+#include "../../inc/parser.h"
 
 bool	handle_exit(t_astnode *head, t_data *data)
 {
-	return (0);
+	int	status;
+
+	status = data->exit_status;
+	if (data->l)
+		free_lexer(data->l);
+	if (data->head)
+		free_ast(&data->head);
+	if (data->env)
+		free_env(&data->env);
+	free(data->line);
+	exit (status);
 }
