e7dec8547012aa4f2acd5e661863b70601664950
Riku Yano <riyano@student.42london.com>
2025-03-19 15:54:38 +0000

    Added sample code for hadle_echo function. Should be modified according to the parser. Argument `data` should be used in the function.

--- src/eval/echo.c
+++ src/eval/echo.c
@@ -15,5 +15,33 @@
 
 bool	handle_echo(t_astnode *head, t_data *data)
 {
+	bool		newline;
+	bool		first_arg;
+	t_astnode	*h;
+
+	if (!head)
+		return (1);
+	newline = true;
+	if (head->children)
+	{
+		if (ft_strncmp(head->children->literal, "-n", 2) == 0)
+		{
+			newline = false;
+			h = head->children->next;
+		}
+		else
+			h = head->children;
+		first_arg = true;
+		while (h)
+		{
+			if (!first_arg)
+				write(STDOUT_FILENO, " ", 1);
+			first_arg = false;
+			write(STDOUT_FILENO, h->literal, ft_strlen(h->literal));
+			h = h->next;
+		}
+	}
+	if (newline)
+		write(STDOUT_FILENO, "\n", 1);
 	return (0);
 }
