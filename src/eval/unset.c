ed0550132b1fe714d9c30273a7fe28ba601d1c34
Riku Yano <riyano@student.42london.com>
2025-03-24 20:30:45 +0000

    Added function for unset command

--- src/eval/unset.c
+++ src/eval/unset.c
@@ -11,8 +11,63 @@
 /* ************************************************************************** */
 
 #include "./eval_private.h"
+#include "../extra/extra.h"
+void	remove_env(t_env **head, char *key)
+{
+	t_env	*prev;
+	t_env	*next;
+	t_env	*target_node;
 
+	if (!head || !*head || !key)
+		return ;
+	target_node = getenv_val(*head, key);
+	if (!target_node)
+		return ;
+	prev = target_node->prev;
+	next = target_node->next;
+	free(target_node->key);
+	free(target_node->value);
+	if (target_node == *head)
+	{
+		free(*head);
+		*head = next;
+		return ;
+	}
+	free(target_node);
+	if (prev)
+		prev->next = next;
+	else if (!next)
+		(*head)->prev = prev;
+}
+
+char	*extract_key(char *arg)
+{
+	char	*equal_sign;
+	char	*key;
+
+	key = arg;
+	equal_sign = ft_strchr(key, '=');
+	if (equal_sign)
+		*equal_sign = '\0';
+	return (key);
+}
+
+#include <stdio.h>
 bool	handle_unset(t_astnode *head, t_data *data)
 {
+	if (!head->children)
+		return (0);
+	//t_astnode *head1 = head;
+	//handle_export(head1, data);
+	char *key = extract_key(head->children->literal);
+	//t_env *cur = data->env;
+	remove_env(&data->env, key);
+	//while (cur)
+	//{
+	//	printf("%s\n", cur->key);
+	//	if (cur->key == key)
+	//		printf("hhhh");
+	//	cur = cur->next;
+	//}
 	return (0);
 }
