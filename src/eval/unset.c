3f964335902f8ef1bbf495005561ba1e0719f86b
Dmytro Melnyk <dmelnyk@student.42london.com>
2025-03-28 20:55:00 +0000

    Unset done

--- src/eval/unset.c
+++ src/eval/unset.c
@@ -14,5 +14,13 @@
 
 bool	handle_unset(t_astnode *head, t_data *data)
 {
+	if (!head || !data || !head->children)
+		return (0);
+	head = head->children;
+	while (head)
+	{
+		env_unset(&data->env, head->literal);
+		head = head->next;
+	}
 	return (0);
 }
