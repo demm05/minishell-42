4f289400f79be0a4ce5b109c627ac7b95a2aee7e
Riku Yano <riyano@student.42london.com>
2025-04-02 18:00:00 +0100

    fix and refacoring for exec_command

--- src/eval/exec_command.c
+++ src/eval/exec_command.c
@@ -6,19 +6,20 @@
 /*   By: dmelnyk <dmelnyk@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/14 15:44:19 by dmelnyk           #+#    #+#             */
-/*   Updated: 2025/03/14 15:47:26 by dmelnyk          ###   ########.fr       */
+/*   Updated: 2025/04/02 17:57:12 by riyano           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "./eval_private.h"
+#include "../extra/extra.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/wait.h>
+#include <sys/stat.h>
 
 static char	**build_envp(t_env *env);
 static char	**build_args(t_astnode *head);
-static char	*get_path(t_env *env, char *literal);
 
 void	exec_command(t_astnode *head, t_data *data)
 {
@@ -32,10 +33,18 @@ void	exec_command(t_astnode *head, t_data *data)
 	signal(SIGQUIT, SIG_DFL);
 	envp = build_envp(data->env);
 	args = build_args(head);
-	path = get_path(getenv_val(data->env, "PATH"), head->literal);
+	path = get_path(getenv_val(data->env, "PATH"), head->literal, data);
+	if (!path)
+	{
+		if (!ft_strchr(head->literal, '/'))
+		{
+			fprintf(stderr, "%s: command not found\n", head->literal);
+			data->exit_status = 127;
+		}
+		exit(data->exit_status);
+	}
 	args[0] = head->literal;
 	execve(path, args, envp);
-	fprintf(stderr, "%s: command not found\n", args[0]);
 	exit(127);
 }
 
@@ -61,11 +70,24 @@ static char	**build_args(t_astnode *head)
 	child = head->children;
 	while (child && i - 1 < head->childs)
 	{
-		args[i++] = child->literal; 
+		args[i++] = child->literal;
 		child = child->next;
 	}
 	args[i] = NULL;
 	return (args);
+}
+
+static int	calc_envsize(t_env *env)
+{
+	int	size;
+
+	size = 0;
+	while (env)
+	{
+		size++;
+		env = env->next;
+	}
+	return (size);
 }
 
 /*
@@ -73,116 +95,25 @@ static char	**build_args(t_astnode *head)
  * Takes a linked list of environment variables ('t_env') and converts it
  * into a dynamically allocated, NULL-terminated array of strings, where each
  * string is in the format "key=value".
- *
- * - Iterates through the 't_env' list to determine the number of environment variables.
- * - Allocates memory for the resulting array of char pointers (plus a NULL terminator).
- * - Iterates through the 't_env' list again, creating a new string for each
- *   key-value pair.  Each string is allocated enough space for the key,
- *   the '=', the value, and a null terminator.
- * - Copies the key, '=', and value into the newly allocated string.
- * - Sets the last element of the array to NULL.
- * - Handles allocation failures by freeing any previously allocated memory and returning NULL.
- * - Returns NULL if the input 'env' is NULL.
  */
 static char	**build_envp(t_env *env)
 {
 	char	**envp;
-	t_env	*penv;
-	int		key_size;
-	int		value_size;
 	int		i;
 	int		size;
-	int		k;
 
 	if (!env)
 		return (NULL);
-	penv = env;
-	size = 0;
-	while (env)
-	{
-		size++;
-		env = env->next;
-	}
+	size = calc_envsize(env);
 	envp = malloc((size + 1) * sizeof(char *));
 	if (!envp)
 		return (NULL);
-	env = penv;
 	i = -1;
 	while (++i < size)
 	{
-		key_size = ft_strlen(env->key);	
-		value_size = ft_strlen(env->value);	
-		envp[i] = malloc(key_size + value_size + 2);
-		if (!envp[i])
-		{
-			while (i > 0)
-				free(envp[--i]);
-			free(envp);
-			return (NULL);
-		}
-		int	j = 0;
-		k = 0;
-		while (k < key_size)
-			envp[i][j++] = env->key[k++];
-		envp[i][j++] = '=';
-		k = 0;
-		while (k < value_size)
-			envp[i][j++] = env->value[k++];
-		envp[i][j] = 0;
+		envp[i] = join_strings(3, env->key, "=", env->value);
 		env = env->next;
 	}
 	envp[size] = NULL;
 	return (envp);
-}
-
-static char	*get_path(t_env *env, char *literal)
-{
-	char	**dir;
-	char	*path;
-	char	*temp_path;
-	int		len;
-	int		i, k;
-	int		dir_len;
-
-	path = NULL;
-	len = 0;
-	if (literal && (literal[0] == '/' || ft_strncmp(literal, "./", 2) == 0))
-		path = ft_strdup(literal);
-	else if (literal && access(literal, X_OK) == 0)
-		path = ft_strdup(literal);
-	else if (!env || !*env->value)
-		dir = ft_split("usr/bin:/bin", ':');
-	else
-		dir = ft_split(env->value, ':');
-	if (!path)
-	{
-		if (literal)
-			len = ft_strlen(literal);
-		i = 0;
-		while (dir[i])
-		{
-			if (!path)
-			{
-				dir_len = ft_strlen(dir[i]);
-				temp_path = malloc(sizeof(char) * (dir_len + len + 2));	
-				int	j = 0;
-				k = 0;
-				while (k < dir_len)
-					temp_path[j++] = dir[i][k++];
-				temp_path[j++] = '/';
-				k = 0;
-				while (k < len)
-					temp_path[j++] = literal[k++];
-				temp_path[j] = 0;
-				if (access(temp_path, X_OK) == 0)
-					path = temp_path;
-				else
-					free(temp_path);
-			}
-			free(dir[i++]);
-		}
-		free(dir);
-	}
-	// TODO: check for execution right and if it's directory
-	return (path);
 }
