0b09a5dd4f0f85923c8c2f7e2003af7007be5cdc
demm05 <179873458+demm05@users.noreply.github.com>
2025-04-03 19:49:04 +0100

    Revert "fixed error handling for command path"

--- src/eval/exec_command.c
+++ src/eval/exec_command.c
@@ -15,14 +15,12 @@
 #include <sys/stat.h>
 
 static char	**build_args(t_astnode *head);
-static void	handle_error(char *literal, t_env *path_env, t_data *data);
 
 void	exec_command(t_astnode *head, t_data *data)
 {
 	char	**envp;
 	char	**args;
 	char	*path;
-	t_env	*path_env;
 
 	if (!*head->literal)
 		exit(0);
@@ -30,10 +28,16 @@ void	exec_command(t_astnode *head, t_data *data)
 	signal(SIGQUIT, SIG_DFL);
 	envp = env_create_arr(data->env);
 	args = build_args(head);
-	path_env = env_get_bykey(data->env, "PATH");
-	path = get_path(path_env, head->literal, data);
+	path = get_path(env_get_bykey(data->env, "PATH"), head->literal, data);
 	if (!path)
-		handle_error(head->literal, path_env, data);
+	{
+		if (!ft_strchr(head->literal, '/'))
+		{
+			fprintf(stderr, "%s: command not found\n", head->literal);
+			data->exit_status = 127;
+		}
+		exit(data->exit_status);
+	}
 	args[0] = head->literal;
 	execve(path, args, envp);
 	free_everything(data);
@@ -67,17 +71,4 @@ static char	**build_args(t_astnode *head)
 	}
 	args[i] = NULL;
 	return (args);
-}
-
-static void	handle_error(char *literal, t_env *path_env, t_data *data)
-{
-	if (!ft_strchr(literal, '/'))
-	{
-		if (!path_env || !path_env->value || !path_env->value[0])
-			fprintf(stderr, "%s: No such file or directory\n", literal);
-		else
-			fprintf(stderr, "%s: command not found\n", literal);
-		data->exit_status = 127;
-	}
-	exit(data->exit_status);
 }
